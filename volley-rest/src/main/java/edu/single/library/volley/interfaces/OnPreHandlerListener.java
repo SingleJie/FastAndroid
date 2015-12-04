15d3e6ade7f32ed30e62bffdafeb2819a3125787
SingleJie <522922030@qq.com>
2015-12-04 17:31:44 +0800

    删除Volley lib

--- volley-rest/src/main/java/edu/single/library/volley/interfaces/OnPreHandlerListener.java
+++
@@ -1,9 +0,0 @@
-package edu.single.library.volley.interfaces;
-
-/**
- * Created by Administrator on 2015/10/7.
- */
-public interface OnPreHandlerListener {
-
-    String onPreHandler(String response);
-}
